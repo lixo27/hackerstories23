c5aa1614cf317f39d45fb184d379740e821b07a9
Thiago Senna <trsenna@gmail.com>
2023-05-09 18:01:32 +0000

    Apply more modular components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
+const welcome = {
+  greeting: 'Hey',
+  title: 'React'
+}
+
+const Greeting = () => {
+  return (
+    <h1>{welcome.greeting}, {welcome.title}!</h1>
+  )
+}
+
+const Search = () => {
+  return (
+    <>
+      <label htmlFor="search">Search:</label>
+      <input id="search" type="text"></input>
+    </>
+  )
+}
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello, React!</h1>
-    </div>
+    <>
+      <Greeting/>
+      <Search />
+    </>
   )
 }
