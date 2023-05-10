c93c874af41efc25f2a321726668c24fa45c58dd
Thiago Senna <trsenna@gmail.com>
2023-05-10 11:59:20 +0000

    Add list of items

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,67 @@
+const item_list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  }
+]
+
 const welcome = {
   greeting: 'Hey',
   title: 'React'
 }
 
-const Greeting = () => {
-  return (
-    <h1>{welcome.greeting}, {welcome.title}!</h1>
-  )
-}
+const Greeting = () => (
+  <h1>{welcome.greeting}, {welcome.title}!</h1>
+)
 
-const Search = () => {
+const Search = () => (
+  <>
+    <label htmlFor="search">Search:</label>
+    <input id="search" type="text"></input>
+  </>
+)
+
+const List = () => (
+  <ul>
+    {item_list.map((item) => {
+      return (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </li>
+      )
+    })}
+  </ul>
+)
+
+const Page = () => {
   return (
     <>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text"></input>
+      <Greeting />
+      <Search />
+
+      <hr />
+      <List />
     </>
   )
 }
 
 export default function App() {
-  return (
-    <>
-      <Greeting/>
-      <Search />
-    </>
-  )
+  return <Page />
 }
